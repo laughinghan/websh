9091d072153db2f2b0aa57c269773bc932ca5711
Angie Song <stud@ee122.(none)>
2011-04-03 07:33:49 -0700

    screen stuff added

---
+++ screen.js
@@ -0,0 +1,32 @@
+/**
+ * Screen
+ */
+
+
+/**
+ * goto_coord
+ *      move the cursor to the given coord
+ */
+function goto_coord(x, y) {
+    var cursor = document.elementFromPoint(x,y);
+    return cursor;
+};
+
+/**
+ * write
+ *      write text to where the cursor is
+ *      either inserts or replaces texted depending on overwrite value
+ */
+function write (str, overwrite) {
+    if(overwrite){
+        ;
+    } else {
+        ;
+    }
+};
+
+
+
+$(function(){ // on load
+
+});
