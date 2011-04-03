0d3ed8e8165a9246a1960b857a904b397221aff0
Angie Song <stud@ee122.(none)>
2011-04-03 09:16:57 -0700

    It blinks

--- screen.js
+++ screen.js
@@ -7,9 +7,13 @@
  * goto_coord
  *      move the cursor to the given coord
  */
-function goto_coord(x, y) {
-    var cursor = document.elementFromPoint(x,y);
-    return cursor;
+function setCursorCoords(_top, _left) {
+    Terminal.top  = _top;
+    Terminal.left = _left;
+};
+
+function getCursorCoords() {
+    return {top: Terminal.top, left: Terminal.left};
 };
 
 /**
@@ -25,8 +29,36 @@ function write (str, overwrite) {
     }
 };
 
+var Terminal = {
+    buffer: "",
+    top: 0,
+    left: 0,
+    history: 0,
+    historyPos: 0,
+    promptActive: true,
+    _cursorBlinkTimeout: null,
+    config: {
+        cursor_blink_time: 500
+    },
 
+
+    
+    init: function() {
+        this.setCursorState();
+    },
+    
+    setCursorState: function() {
+        // toggle cursor
+        $("#cursor").toggleClass("on").toggleClass("off");
+
+        // blink cursor
+        this._cursorBlinkTimeout = window.setTimeout($.proxy(function(){
+            this.setCursorState(!this.cursorBlinkState,true)
+        }, this), this.config.cursor_blink_time);
+        
+    }
+};
 
 $(function(){ // on load
-
+    Terminal.init();
 });
